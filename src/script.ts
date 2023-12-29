b794b3e343a6490125b3438f46b566ab77ebb780
Alex Leandro <alexorcizamus@gmail.com>
2023-12-29 20:35:26 -0300

    feat: normalizarTransacao module :sparkles:

--- src/script.ts
+++ src/script.ts
@@ -1,34 +1,13 @@
 import fetchData from "./fetchData.js";
-
-type TransacaoPagamento = "Cartão de Crédito" | "Boleto";
-type TransacaoStatus =
-  | "Paga"
-  | "Recusada pela operadora de cartão"
-  | "Aguardando pagamento"
-  | "Estornada";
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ["Valor (R$)"]: string;
-  ["Forma de Pagamento"]: TransacaoPagamento;
-  ["Cliente Novo"]: number;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     "https://api.origamid.dev/json/transacoes.json"
   );
+  if (!data) return;
 
-  if (data) {
-    data.forEach((item) => {
-      console.log(item["Valor (R$)"]);
-    });
-  }
-
-  console.log("Código continuou!");
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
 }
 handleData();
